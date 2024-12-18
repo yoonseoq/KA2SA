b14441d55181c1a1b34e9b28fc7a82b3c4f3c6c6
leejunyoung228 <80664840+leejunyoung228@users.noreply.github.com>
2024-12-18 14:59:06 +0900

    2024-12-18 14:58 swagger update

--- src/main/java/com/green/ca2sa/email/EmailController.java
+++ src/main/java/com/green/ca2sa/email/EmailController.java
@@ -3,6 +3,8 @@ package com.green.ca2sa.email;
 import com.green.ca2sa.common.model.ResultResponse;
 import com.green.ca2sa.email.model.EmailSendCodeReq;
 import com.green.ca2sa.email.model.EmailVerifyCodeReq;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
@@ -11,11 +13,13 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("email-auth")
+@Tag(name = "이메일 인증")
 public class EmailController {
     private final EmailService emailService;
     private final AuthCodeService authCodeService;
 
     @PostMapping("send-code")
+    @Operation(description = "이메일 인증 코드 발송")
     public ResultResponse<Boolean> postSendCode(@RequestBody EmailSendCodeReq p) {
         String code = authCodeService.generateAuthCode(p);
         boolean result = emailService.sendCodeToEmail(p.getEmail(), "이메일 인증 코드", code);
@@ -26,6 +30,7 @@ public class EmailController {
     }
 
     @PostMapping("verify-code")
+    @Operation(description = "이메일 인증 코드 검사")
     public ResultResponse<Boolean> postVerifyCode(@RequestBody EmailVerifyCodeReq p) {
         boolean result = authCodeService.validateAuthCode(p);
         return ResultResponse.<Boolean>builder()
