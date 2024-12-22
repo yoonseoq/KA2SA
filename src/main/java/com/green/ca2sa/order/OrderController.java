0d0fbdc76698fad0270775804046eda8f6af5ebf
yoonseoq <sysooo18@naver.com>
2024-12-22 16:15:33 +0900

    241222 16:15 그냥 OrderController 각 매핑 명칭설정

--- src/main/java/com/green/ca2sa/order/OrderController.java
+++ src/main/java/com/green/ca2sa/order/OrderController.java
@@ -4,6 +4,7 @@ import com.green.ca2sa.common.model.ResultResponse;
 import com.green.ca2sa.order.model.OrderGetReq;
 import com.green.ca2sa.order.model.OrderGetRes;
 import com.green.ca2sa.order.model.OrderPostReq;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -19,6 +20,7 @@ public class OrderController {
     private final OrderService orderService;
 
     @PostMapping
+    @Operation(description = "주문 하기")
     public ResultResponse<Integer> postOrder(@RequestBody OrderPostReq p) {
         int result = orderService.PostOrder(p);
         log.info("OrderController > postOrder > req: {}", p);
@@ -29,6 +31,7 @@ public class OrderController {
     }
 
     @GetMapping
+    @Operation(description = "주문 확인")
     public ResultResponse<List<OrderGetRes>> getOrderList(OrderGetReq p) {
         List<OrderGetRes> list = orderService.GetOrderList(p);
         return ResultResponse.<List<OrderGetRes>>builder()
