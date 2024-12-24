7e090b17871110f9e7a7fe9712b834f7f3bcb845
leejunyoung228 <80664840+leejunyoung228@users.noreply.github.com>
2024-12-24 17:41:37 +0900

    2024-12-24 17:41

--- src/main/java/com/green/ca2sa/common/WebMvcConfiguration.java
+++ src/main/java/com/green/ca2sa/common/WebMvcConfiguration.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.method.HandlerTypePredicate;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -25,13 +24,5 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
     @Override
     public void configurePathMatch(PathMatchConfigurer configurer) {
         configurer.addPathPrefix("api", HandlerTypePredicate.forAnnotation(RestController.class));
-    }
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
-                .allowedOrigins("*")
-                .allowedMethods("PUT", "GET", "POST", "DELETE", "PATCH")
-                .allowedHeaders("*");
     }
 }
